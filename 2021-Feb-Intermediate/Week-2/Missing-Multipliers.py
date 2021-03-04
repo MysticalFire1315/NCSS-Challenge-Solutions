4704c3b8514eb1c5e56046f11022301add820cb4
MysticalFire1315 <80010607+MysticalFire1315@users.noreply.github.com>
2021-03-04 11:28:59 +1100

    Delete 2021-Feb-Intermediate directory
    Deleted to reorganize directory

--- 2021-Feb-Intermediate/Week-2/Missing-Multipliers.py
+++
@@ -1,9 +0,0 @@
-def main():
-    table = int(input("Times table: "))
-    step = int(input("Step: "))
-    for num in range(3, 13, step):
-        print(f"{table} x [ ] = {table*num}")
-    return
-
-if __name__ == "__main__":
-    main()
